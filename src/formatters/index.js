0d054332bc943419741a8d9afe48946f4c31743b
BudenchukM <budenchuk.marina@gmail.com>
2025-06-25 22:51:36 +0300

    13

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,21 +1,21 @@
-import formatStylish from './stylish.js';
-import formatPlain from './plain.js';
-import formatJson from './json.js';
+import formatStylish from './stylish.js'
+import formatPlain from './plain.js'
+import formatJson from './json.js'
 
 const formatters = {
   stylish: formatStylish,
   plain: formatPlain,
-  json: formatJson
+  json: formatJson,
 };
 
 const getFormatter = (formatName) => {
   if (typeof formatName !== 'string') {
-    throw new Error(`Format name must be a string, received: ${typeof formatName}`);
+    throw new Error(`Format name must be a string, received: ${typeof formatName}`)
   }
-  const formatter = formatters[formatName];
+  const formatter = formatters[formatName]
   if (!formatter) {
-    throw new Error(`Unknown format: ${formatName}`);
+    throw new Error(`Unknown format: ${formatName}`)
   }
-  return formatter;
-};
-export default getFormatter;
+  return formatter
+}
+export default getFormatter
