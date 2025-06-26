c313161971fc29997301ae50c8a3f7158ea4084a
BudenchukM <budenchuk.marina@gmail.com>
2025-06-26 13:59:24 +0300

    src/index.js2

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import parse from './parsers.js'
 import buildDiff from './diffBuilder.js'
 import getFormatter from './formatters/index.js'
-import formatPlain from './formatters/plain.js'
-import formatJson from './formatters/json.js'
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
   if (typeof formatName !== 'string') {
     throw new Error(`Format name must be a string, received: ${typeof formatName}`)
   }
+  
   const formatter = getFormatter(formatName)
   const data1 = parse(filepath1)
   const data2 = parse(filepath2)
   const diff = buildDiff(data1, data2)
-  switch (formatter) {
-    case 'plain':
-      return formatPlain(diff)
-    case 'json':
-      return formatJson(diff)
-    case 'stylish':
-    default:
-      return formatter(diff)
-  }
+  
+  return formatter(diff)
 }
