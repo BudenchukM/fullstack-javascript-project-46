b7976d97d30a7b822716ca4ee80cf8f58b64452e
BudenchukM <budenchuk.marina@gmail.com>
2025-06-26 14:01:52 +0300

    lint

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ export default (filepath1, filepath2, formatName = 'stylish') => {
   if (typeof formatName !== 'string') {
     throw new Error(`Format name must be a string, received: ${typeof formatName}`)
   }
-  
   const formatter = getFormatter(formatName)
   const data1 = parse(filepath1)
   const data2 = parse(filepath2)
   const diff = buildDiff(data1, data2)
-  
   return formatter(diff)
 }
